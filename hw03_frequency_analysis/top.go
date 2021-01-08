0f653bab1b89c36d3880328bc52f33cfdfd166b2
Ruslan <sabirov@worksolutions.ru>
2021-01-08 13:57:29 +0300

    HW3 Реализация частотного анализатора

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,40 @@
 package hw03_frequency_analysis //nolint:golint,stylecheck
 
-func Top10(_ string) []string {
-	// Place your code here
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+const topLen = 10
+
+func Top10(value string) []string {
+	var top = make([]string, 0, topLen)
+	if len(value) == 0 {
+		return top
+	}
+
+	words := regexp.MustCompile("[ \n\t]").Split(value, -1)
+	var dict = make(map[string]int)
+	for _, s := range words {
+		s = strings.Trim(s, " ")
+		if len(s) == 0 {
+			continue
+		}
+		dict[s]++
+	}
+
+	for k := range dict {
+		top = append(top, k)
+	}
+
+	sort.Slice(top, func(i, j int) bool {
+		return dict[top[i]] > dict[top[j]]
+	})
+
+	if len(top) > topLen {
+		return top[:topLen]
+	}
+
+	return top
 }
