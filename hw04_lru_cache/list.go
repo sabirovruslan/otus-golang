6b506f3f92547c95d2e6a9830b3f944505a72c70
Ruslan <sabirov@worksolutions.ru>
2021-01-19 22:32:52 +0300

    HW4 LRU-кэш. Реализация интерфейса List

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,15 +1,52 @@
 package hw04_lru_cache //nolint:golint,stylecheck
 
 type List interface {
-	// Place your code here
+	Len() int
+	Front() *listItem
+	Back() *listItem
+	PushFront(v interface{}) *listItem
+	PushBack(v interface{}) *listItem
+	Remove(i *listItem)
+	MoveToFront(i *listItem)
 }
 
 type listItem struct {
-	// Place your code here
+	Value interface{}
+	Next  *listItem
+	Prev  *listItem
 }
 
 type list struct {
-	// Place your code here
+	front *listItem
+	back  *listItem
+}
+
+func (l list) Len() int {
+	return 0
+}
+
+func (l list) Front() *listItem {
+	return l.front
+}
+
+func (l list) Back() *listItem {
+	return l.back
+}
+
+func (l list) PushFront(v interface{}) *listItem {
+
+}
+
+func (l list) PushBack(v interface{}) *listItem {
+
+}
+
+func (l list) Remove(i *listItem) {
+
+}
+
+func (l list) MoveToFront(i *listItem) {
+
 }
 
 func NewList() List {
