2c8e047d62f62f75f064410dc1c8f053ad11b80b
Ruslan <sabirov@worksolutions.ru>
2021-01-23 19:46:40 +0300

    HW4 LRU-кэш. Реализация интерфейса List

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,38 +17,101 @@ type listItem struct {
 }
 
 type list struct {
-	front *listItem
-	back  *listItem
-}
-
-func (l list) Len() int {
-	return 0
-}
-
-func (l list) Front() *listItem {
-	return l.front
-}
-
-func (l list) Back() *listItem {
-	return l.back
-}
-
-func (l list) PushFront(v interface{}) *listItem {
-
-}
-
-func (l list) PushBack(v interface{}) *listItem {
-
-}
-
-func (l list) Remove(i *listItem) {
-
-}
-
-func (l list) MoveToFront(i *listItem) {
-
+	tail *listItem
+	size int
 }
 
 func NewList() List {
 	return &list{}
+}
+
+func (l *list) Len() int {
+	return l.size
+}
+
+func (l *list) Front() *listItem {
+	if l.tail == nil {
+		return l.tail
+	}
+
+	front := l.tail
+	for front.Prev != nil {
+		front = front.Prev
+	}
+	return front
+}
+
+func (l *list) Back() *listItem {
+	return l.tail
+}
+
+func (l *list) PushFront(v interface{}) *listItem {
+	defer func() {
+		l.size++
+	}()
+
+	item := &listItem{v, nil, nil}
+	if l.tail == nil {
+		l.tail = item
+		return l.tail
+	}
+
+	front := l.tail
+	for front.Prev != nil {
+		front = front.Prev
+	}
+	front.Prev, item.Next = item, front
+
+	return l.tail
+}
+
+func (l *list) PushBack(v interface{}) *listItem {
+	defer func() {
+		l.size++
+	}()
+
+	item := &listItem{v, nil, l.tail}
+	if l.tail == nil {
+		l.tail = item
+		return l.tail
+	}
+
+	l.tail.Next, item.Prev = item, l.tail
+	l.tail = item
+
+	return l.tail
+}
+
+func (l *list) Remove(i *listItem) {
+	defer func() {
+		l.size--
+	}()
+
+	if i.Prev == nil && i.Next == nil {
+		return
+	}
+
+	if i.Next == nil {
+		i.Prev.Next = nil
+		l.tail = i.Prev
+		return
+	}
+
+	i.Prev.Next, i.Next.Prev = i.Next, i.Prev
+}
+
+func (l *list) MoveToFront(i *listItem) {
+	if i.Prev == nil {
+		return
+	}
+
+	v := i.Prev
+	for v.Prev != nil {
+		v = v.Prev
+	}
+
+	l.Remove(i)
+
+	i.Next, v.Prev = v, i
+	i.Prev = nil
 }
